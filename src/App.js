8d6977095b08739fe288022db425b4687f47d51e
Victoria Kush <victoria.v.kush@gmail.com>
2020-08-20 10:42:15 +0200

    ok

--- src/App.js
+++ src/App.js
@@ -1,28 +1,37 @@
 import React from 'react';
 import Iframe from 'react-iframe'
 import 'font-awesome/css/font-awesome.min.css';
-import logoSvg from './img/js.svg';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faLinkedin, faHtml5, faCss3Alt, faJs, faReact } from '@fortawesome/free-brands-svg-icons';
+import { faPhoneAlt, faDatabase, faEnvelope } from "@fortawesome/free-solid-svg-icons";
+import { dom } from '@fortawesome/fontawesome-svg-core';
 import './App.css';
 import './my-cv.css';
+
+dom.watch()
+
+library.add(
+  faPhoneAlt, faEnvelope, faLinkedin, faHtml5, faCss3Alt, faJs, faReact, faDatabase
+);
 
 function App() {
   return (
     <div className="main-container">
       <header className="header-container">
         <div className="profil-foto">
-          <img src="https://secure.gravatar.com/avatar/3da283f44646867ed9c7e7e022340300?size=150" alt="foto"/>
+          <img src="https://secure.gravatar.com/avatar/3da283f44646867ed9c7e7e022340300?size=150" alt="foto" />
         </div>
         <div className="main-info">
           <h1>Victoria Kush</h1>
           <ul>
             <li>
-              <i className='fa fa-phone'></i><a href="tel:71465372" title="nummer of the phone">71 46 53 72</a>
+              <i className='fas fa-phone-alt'></i><a href="tel:71465372" title="nummer of the phone">71 46 53 72</a>
             </li>
             <li>
-              <i className='fa fa-envelope'></i><a href="mailto:victoria.v.kush@gmail.com?" title="e-mail" subject="subject text">victoria.v.kush@gmail.com</a>
+              <i className='fas fa-envelope'></i><a href="mailto:victoria.v.kush@gmail.com?" title="e-mail" subject="subject text">victoria.v.kush@gmail.com</a>
             </li>
             <li>
-              <i className='fa fa-linkedin'></i><a href="https://www.linkedin.com/in/victoria-kush-4a0455155" title="profil of the linkedin account">https://www.linkedin.com/in/victoria-kush-4a0455155/</a>
+              <i class="fab fa-linkedin"></i><a href="https://www.linkedin.com/in/victoria-kush-4a0455155" title="profil of the linkedin account">https://www.linkedin.com/in/victoria-kush-4a0455155/</a>
             </li>
           </ul>
         </div>
@@ -33,8 +42,16 @@ function App() {
       </section>
       <section className="experience">
         <h2>EXPERIENCE</h2>
+        <h3>Front-end Web Developer Self-Employed</h3>
+        <p><i>From Februar 2019</i></p>
+        <ul>
+          <li>Learned HTML / CSS / JavaScript / React</li>
+          <li>Built a responsive website (link below) with animations and interactivity</li>
+          <li>Built  (link below)</li>
+          <li>Built a weather app (link below) based on user input</li>
+        </ul>
         <h3>Statistic Assistant in Åben Pårørenderådgivning SIND i København.</h3>
-        <p><i>From Februar 2018</i></p>
+        <p><i>Februar 2018 - Februar 2019</i></p>
         <p>I made statistical work and led statistics of both evaluations of interviews and records of inquiries. I have used my computer skills and knowledge of MS Office applications especially MS Excel. Statistical work was done for the 6 month of work of the ÅBEN PÅRØRENDERÅDGIVNING, that show the effectiveness of the work of consultants and allowed to make changes to further improve their work.</p>
         <h3>Lecturer, Institit Psychology V. N. Karazin Kharkiv National University.</h3>
         <p><i>September 2009 – Januar 2015</i></p>
@@ -64,16 +81,18 @@ function App() {
       <section className="skills">
         <h2>SKILLS</h2>
         <ul>
-          <li><i className="fa fa-html5"></i>HTML</li>
-          <li><i className='fa fa-css3'></i>CSS</li>
-          <li className="logo-js">
-            <img src={logoSvg} alt="logo js" />
-            JavaScript
-          </li>
+          <li><i class="fab fa-html5"></i>HTML</li>
+          <li><i class="fab fa-css3-alt"></i>CSS</li>
+          <li><i class="fab fa-js"></i>JavaScript</li>
+          <li><i class="fab fa-react"></i>React</li>
+          <li><i class="fas fa-database"></i>Database</li>
         </ul>
       </section>
       <section className="education">
         <h2>EDUCATION</h2>
+        <h3>Full Stack Web Development in HackYourFuture - Copenhagen</h3>
+        <p><i>2020</i></p>
+        <p>The curriculum includes HTML/CSS/Git/CLI/Javascript/NodeJS/ExpressJS/React etc.</p>
         <h3>Ph.d. in  Psychology V. N. Karazin Kharkiv National University.</h3>
         <p><i>2008 – 2012</i></p>
         <h3>Master's Degree in Psychology V. N. Karazin Kharkiv National University.</h3>
@@ -82,22 +101,14 @@ function App() {
         <p><i>2003-2008</i></p>
       </section>
       <section className="motivation">
-        <h2>Motivation and learning strategies</h2>
+        <h2>CERTIFICATES</h2>
         <div className="learning">
-          <ul className="column column-1">
-            <li>Use the right HTML element at the right place</li>
-            <li>Don’t use divs for everything</li>
-            <li>Think about CSS as reusable components</li>
-          </ul>
-          <div className="column">
-            <Iframe  className="video" url="https://www.youtube.com/embed/CfeRThEQFFE" title="Coding Motivational Video"/>
-          </div>
           <ul className="column">
-            <li>Define utilities in CSS to dry up CSS</li>
-            <li>Care about the front-end code, take ownership of it, and always continue to improve upon it</li>
+            <li>Responsive Web Design Certification by freeCodeCamp</li>
+            <li>JavaScript Algorithms and Data Structures Certification by freeCodeCamp</li>
           </ul>
         </div>
-      </section>  
+      </section>
       <footer>
         <section className="copyright">
           © 2020 Victoria Kush
